8ef692b621ccb0a89b45c85f1668ab143c00c960
Pr1ntel <vandushev.sergey@yandex.ru>
2023-05-19 00:46:26 +0300

    add description

--- src/components/secure/StyleFilms.jsx
+++ src/components/secure/StyleFilms.jsx
@@ -1,89 +1,173 @@
-import {Table, Col, Row} from "antd";
-import {useNavigate} from "react-router-dom"
-import React from "react";
-
-function StyleFilms() {
+import { SearchOutlined } from '@ant-design/icons';
+import { Button, Input, Space, Table } from 'antd';
+import { useRef, useState } from 'react';
+import Highlighter from 'react-highlight-words';
+const data = [
+    {
+        key: '1',
+        name: 'John Brown',
+        age: 1,
+        address: 'New York No. 1 Lake Park',
+        description:'asdasdasda',
+    },
+    {
+        key: '2',
+        name: 'Joe Black',
+        age: 2,
+        address: 'London No. 1 Lake Park',
+    },
+    {
+        key: '3',
+        name: 'Jim Green',
+        age: 3,
+        address: 'Sydney No. 1 Lake Park',
+    },
+    {
+        key: '4',
+        name: 'Jim Red',
+        age: 4,
+        address: 'London No. 2 Lake Park',
+    },
+];
+const StyleFilms = () => {
+    const [searchText, setSearchText] = useState('');
+    const [searchedColumn, setSearchedColumn] = useState('');
+    const searchInput = useRef(null);
+    const handleSearch = (selectedKeys, confirm, dataIndex) => {
+        confirm();
+        setSearchText(selectedKeys[0]);
+        setSearchedColumn(dataIndex);
+    };
+    const handleReset = (clearFilters) => {
+        clearFilters();
+        setSearchText('');
+    };
+    const getColumnSearchProps = (dataIndex) => ({
+        filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
+            <div
+                style={{
+                    padding: 8,
+                }}
+                onKeyDown={(e) => e.stopPropagation()}
+            >
+                <Input
+                    ref={searchInput}
+                    placeholder={`Search ${dataIndex}`}
+                    value={selectedKeys[0]}
+                    onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+                    onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+                    style={{
+                        marginBottom: 8,
+                        display: 'block',
+                    }}
+                />
+                <Space>
+                    <Button
+                        type="primary"
+                        onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+                        icon={<SearchOutlined />}
+                        size="small"
+                        style={{
+                            width: 90,
+                        }}
+                    >
+                        Search
+                    </Button>
+                    <Button
+                        onClick={() => clearFilters && handleReset(clearFilters)}
+                        size="small"
+                        style={{
+                            width: 90,
+                        }}
+                    >
+                        Reset
+                    </Button>
+                    <Button
+                        type="link"
+                        size="small"
+                        onClick={() => {
+                            confirm({
+                                closeDropdown: false,
+                            });
+                            setSearchText(selectedKeys[0]);
+                            setSearchedColumn(dataIndex);
+                        }}
+                    >
+                        Filter
+                    </Button>
+                    <Button
+                        type="link"
+                        size="small"
+                        onClick={() => {
+                            close();
+                        }}
+                    >
+                        close
+                    </Button>
+                </Space>
+            </div>
+        ),
+        filterIcon: (filtered) => (
+            <SearchOutlined
+                style={{
+                    color: filtered ? '#1890ff' : undefined,
+                }}
+            />
+        ),
+        onFilter: (value, record) =>
+            record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+        onFilterDropdownOpenChange: (visible) => {
+            if (visible) {
+                setTimeout(() => searchInput.current?.select(), 100);
+            }
+        },
+        render: (text) =>
+            searchedColumn === dataIndex ? (
+                <Highlighter
+                    highlightStyle={{
+                        backgroundColor: '#ffc069',
+                        padding: 0,
+                    }}
+                    searchWords={[searchText]}
+                    autoEscape
+                    textToHighlight={text ? text.toString() : ''}
+                />
+            ) : (
+                text
+            ),
+    });
     const columns = [
         {
             title: 'Название',
             dataIndex: 'name',
             key: 'name',
+            width: '30%',
+            ...getColumnSearchProps('name'),
         },
         {
-            title: 'Дата выхода',
+            title: 'Длительность',
             dataIndex: 'age',
             key: 'age',
+            width: '10%',
+            sorter: (a, b) => a.age > b.age,   /*поправить соритровку*/
+            sortDirections: ['descend', 'ascend'],
         },
         {
-            title: 'Длительность(Минут)',
+            title: 'Address',
             dataIndex: 'address',
             key: 'address',
+            ...getColumnSearchProps('address'),
+            sorter: (a, b) => a.address.length - b.address.length,
+            sortDirections: ['descend', 'ascend'],
         },
         {
-            title: 'Фото',
-            dataIndex: 'photo',
-            key: 'photo',
-        },
-
-    ];
-    const data = [
-        {
-            key: 1,
-            name: 'Фильм1',
-            age: '18.2.1990',
-            address: '190',
-            description: 'Тут какое то описание',
-        },
-        {
-            key: 2,
-            name: 'Фильм2',
-            age: '18.2.1990',
-            address: '180',
-            description: 'описание',
-        },
-        {
-            key: 3,
-            name: 'Фильм3',
-            age: '18.2.1990',
-            address: '177',
-            description: 'Описание',
-        },
-        {
-            key: 4,
-            name: 'Фильм4',
-            age: '18.2.1990',
-            address: '156',
-            description: 'Описание',
+            title: 'Описание',
+            dataIndex: 'description',
+            key: 'description',
+            width: '20%',
+            ...getColumnSearchProps('name'),
         },
     ];
-    const navigate = useNavigate()
-    return (
-        <div>
-            <Row>
-                <Col span={24}>
-                    <div style={{opacity: 0.9}}>
-                        <Table
-                            columns={columns}
-                            expandable={{
-                                expandedRowRender: (record) => (
-                                    <p
-                                        style={{
-                                            margin: 0,
-                                        }}
-                                    >
-                                        {record.description}
-                                    </p>
-                                ),
-                                rowExpandable: (record) => record.name !== 'Not Expandable',
-                            }}
-                            dataSource={data}
-
-                        />
-                    </div>
-                </Col>
-            </Row>
-        </div>
-    )
-}
-
+    return <Table columns={columns} dataSource={data} />;
+};
 export default StyleFilms;
